9dceabcd6ff25ef2fe76a0d2b365083ab635138b
omm_prakash_nayak <ommprakashnayakkurujanga@gmail.com>
2025-11-09 10:04:35 +0530

    add a multiplication file

---
+++ 2.c
@@ -0,0 +1,20 @@
+//BRANCH 2 ADDING MULTIPLICATION FUNCTION
+#include<stdio.h>
+int mult(int ,int);
+int main()
+{
+
+int n1,n2;
+printf("enter two number\n");
+
+
+scanf("%d%d",&n1,&n2);
+printf("%d * %d = %d\n",n1,n2,mult(n1,n2));
+
+return 0;
+}
+
+int mult(int n1,int n2)
+{
+return n1 * n2;
+}
